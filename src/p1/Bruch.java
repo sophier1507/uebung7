b7d78133e0962445fbe58b74a18eac90a3490798
sophier1507 <sophieroesgen@gmail.com>
2025-12-01 12:15:51 +0100

    p1

---
+++ src/p1/Bruch.java
@@ -0,0 +1,16 @@
+package p1;
+
+public class Bruch {
+	int z;
+	int n;
+	
+	public Bruch(int zaehler, int nenner) {
+		z = zaehler;
+		n = nenner;
+	}
+	
+	public String toString() {
+		return z + ":" + n;
+	}
+}
+
---
+++ src/p1/P1_main.java
@@ -0,0 +1,28 @@
+package p1;
+
+public class P1_main {
+
+	public static void main(String[] args) {
+		multFrac(5,2,3,4);
+		Bruch b1 = new Bruch(5,2);
+		Bruch b2 = new Bruch(3,4);
+		multFrac(b1,b2);
+		b1.toString();
+		b2.toString();
+		System.out.println(b1);
+		System.out.println(b1.toString());
+
+	}
+
+	public static void multFrac(int z1, int n1, int z2, int n2) {
+		int z = z1 * z2;
+		int n = n1 * n2;
+		System.out.println(z + ":" + n);
+	}
+	
+	public static void multFrac(Bruch a, Bruch b) {
+		int z = a.z * b.z;
+		int n = a.n * b.n;
+		System.out.println(z + ":" + n);
+	}
+}
